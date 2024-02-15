b058cdc420bddd903a436720d12ff40791aaca19
Greg Weaver <greg.weaver@careevolution.com>
2024-02-15 12:14:08 -0500

    chanmges for neo motors

--- src/main/java/frc/robot/subsystems/IntakeSubsystem.java
+++ src/main/java/frc/robot/subsystems/IntakeSubsystem.java
@@ -1,19 +1,16 @@
 package frc.robot.subsystems;
 
-import com.ctre.phoenix.motorcontrol.SupplyCurrentLimitConfiguration;
-import com.ctre.phoenix.motorcontrol.TalonFXControlMode;
-import com.ctre.phoenix.motorcontrol.can.TalonFX;
-import com.ctre.phoenix.motorcontrol.can.TalonFXConfiguration;
-import com.ctre.phoenix.sensors.SensorInitializationStrategy;
+
+import com.revrobotics.CANSparkMax;
+import com.revrobotics.CANSparkMaxLowLevel.MotorType;
 
 import edu.wpi.first.wpilibj2.command.SubsystemBase;
-import frc.robot.Constants;
+
 
 public class IntakeSubsystem extends SubsystemBase {
-  private TalonFX _intakeMotor;
+  private CANSparkMax _intakeMotor;
   public IntakeSubsystem(int INTAKE_MOTOR) {
-    _intakeMotor = new TalonFX(INTAKE_MOTOR);
-    configMotor(_intakeMotor);
+    _intakeMotor = new CANSparkMax(INTAKE_MOTOR,MotorType.kBrushless);
   }
 
   public void intakeOn() {
@@ -24,39 +21,14 @@ public class IntakeSubsystem extends SubsystemBase {
   }
   
   public void intake(double speed) {
-    _intakeMotor.set(TalonFXControlMode.PercentOutput, speed);
+    _intakeMotor.set(speed);
   }
 
   public void discharge(double speed) {
-    _intakeMotor.set(TalonFXControlMode.PercentOutput, -speed);
+    _intakeMotor.set(-speed);
   }
 
   public void move(double speed) {
-    _intakeMotor.set(TalonFXControlMode.PercentOutput, speed);
+    _intakeMotor.set(speed);
   }
-
-  private void configMotor(TalonFX motor) {
-    TalonFXConfiguration talonFXConfig = new TalonFXConfiguration();
-
-    /* Swerve Drive Motor Configuration */
-    SupplyCurrentLimitConfiguration driveSupplyLimit = new SupplyCurrentLimitConfiguration(
-        Constants.SwerveDrivetrain.DRIVE_ENABLE_CURRENT_LIMIT, 
-        Constants.SwerveDrivetrain.DRIVE_CONTINUOUS_CL, 
-        Constants.SwerveDrivetrain.DRIVE_PEAK_CL, 
-        Constants.SwerveDrivetrain.DRIVE_PEAK_CURRENT_DURATION);
-
-    talonFXConfig.slot0.kP = Constants.SwerveDrivetrain.DRIVE_kP;
-    talonFXConfig.slot0.kI = Constants.SwerveDrivetrain.DRIVE_kI;
-    talonFXConfig.slot0.kD = Constants.SwerveDrivetrain.DRIVE_kD;
-    talonFXConfig.slot0.kF = Constants.SwerveDrivetrain.DRIVE_kF;        
-    talonFXConfig.supplyCurrLimit = driveSupplyLimit;
-    talonFXConfig.initializationStrategy = SensorInitializationStrategy.BootToZero;
-    talonFXConfig.openloopRamp = Constants.SwerveDrivetrain.OPEN_LOOP_RAMP;
-    talonFXConfig.closedloopRamp = Constants.SwerveDrivetrain.CLOSED_LOOP_RAMP;
-    
-    motor.configFactoryDefault();
-    motor.configAllSettings(talonFXConfig);
-    //motor.setInverted(Constants.SwerveDrivetrain.DRIVE_MOTOR_INVERTED);
-    motor.setNeutralMode(Constants.SwerveDrivetrain.DRIVE_NEUTRAL_MODE);
-}
 }
--- src/main/java/frc/robot/subsystems/LiftSubsystem.java
+++ src/main/java/frc/robot/subsystems/LiftSubsystem.java
@@ -1,40 +1,16 @@
 package frc.robot.subsystems;
 
-import com.ctre.phoenix.motorcontrol.SupplyCurrentLimitConfiguration;
-import com.ctre.phoenix.motorcontrol.TalonFXControlMode;
-import com.ctre.phoenix.motorcontrol.can.TalonFX;
-import com.ctre.phoenix.motorcontrol.can.TalonFXConfiguration;
-import com.ctre.phoenix.sensors.SensorInitializationStrategy;
+import com.revrobotics.CANSparkMax;
+import com.revrobotics.CANSparkMaxLowLevel.MotorType;
 
 import edu.wpi.first.wpilibj2.command.SubsystemBase;
-import frc.robot.Constants;
 
 public class LiftSubsystem extends SubsystemBase {
   //private WPI_TalonFX _liftMotor;
-  private TalonFX _liftMotor;
+  private CANSparkMax _liftMotor;
   public LiftSubsystem(int lift_motor_id) {
-    _liftMotor = new TalonFX(lift_motor_id);
-    TalonFXConfiguration talonFXConfig = new TalonFXConfiguration();
-
-    /* Swerve Drive Motor Configuration */
-    SupplyCurrentLimitConfiguration driveSupplyLimit = new SupplyCurrentLimitConfiguration(
-        Constants.SwerveDrivetrain.DRIVE_ENABLE_CURRENT_LIMIT, 
-        Constants.SwerveDrivetrain.DRIVE_CONTINUOUS_CL, 
-        Constants.SwerveDrivetrain.DRIVE_PEAK_CL, 
-        Constants.SwerveDrivetrain.DRIVE_PEAK_CURRENT_DURATION);
-
-    talonFXConfig.slot0.kP = Constants.SwerveDrivetrain.DRIVE_kP;
-    talonFXConfig.slot0.kI = Constants.SwerveDrivetrain.DRIVE_kI;
-    talonFXConfig.slot0.kD = Constants.SwerveDrivetrain.DRIVE_kD;
-    talonFXConfig.slot0.kF = Constants.SwerveDrivetrain.DRIVE_kF;        
-    talonFXConfig.supplyCurrLimit = driveSupplyLimit;
-    talonFXConfig.initializationStrategy = SensorInitializationStrategy.BootToZero;
-    talonFXConfig.openloopRamp = Constants.SwerveDrivetrain.OPEN_LOOP_RAMP;
-    talonFXConfig.closedloopRamp = Constants.SwerveDrivetrain.CLOSED_LOOP_RAMP;
-    _liftMotor.configAllSettings(talonFXConfig);
-
-    _liftMotor.setNeutralMode(Constants.SwerveDrivetrain.DRIVE_NEUTRAL_MODE);
-}
+    _liftMotor = new CANSparkMax(lift_motor_id, MotorType.kBrushless);  
+  }
 
 
   public void loadPosition() {
@@ -42,15 +18,14 @@ public class LiftSubsystem extends SubsystemBase {
   }
   
   public void up(double speed) {
-    _liftMotor.set(TalonFXControlMode.PercentOutput, speed);
+    _liftMotor.set(speed);
   }
 
   public void down(double speed) {
-    _liftMotor.set(TalonFXControlMode.PercentOutput, -speed);
+    _liftMotor.set(-speed);
   }
 
   public void move(double speed) {
-    //System.out.println("speed = " + speed);
-    _liftMotor.set(TalonFXControlMode.PercentOutput, speed);
+    _liftMotor.set(speed);
   }
 }
